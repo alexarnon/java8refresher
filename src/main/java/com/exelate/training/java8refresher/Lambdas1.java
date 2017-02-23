228503c5abd23a2a524d4ddefc552ba207ee77e1
alexarnon <alex.arnon@nielsen.com>
2017-02-23 11:23:51 +0200

    Initial.

--- src/main/java/com/exelate/training/java8refresher/Lambdas1.java
+++
@@ -1,6 +0,0 @@
-package com.exelate.training.java8refresher;
-
-@SuppressWarnings({"StaticPseudoFunctionalStyleMethod", "ConstantConditions", "Convert2MethodRef"})
-public class Lambdas1 {
-
-}
--- src/test/java/com/exelate/training/java_8_refresher/OptionalWorkshop.java
+++
@@ -1,7 +0,0 @@
-package com.exelate.training.java_8_refresher;
-
-import java.util.Optional;
-
-public class OptionalWorkshop {
-
-}
